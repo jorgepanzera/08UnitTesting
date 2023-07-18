8a745ab88a131cef5c5e5de0f8b8c5e2926ae689
Jorge Panzera <panzera.jorge@gmail.com>
2023-07-18 20:29:31 -0300

    primeros cases

---
+++ src/funciones.js
@@ -0,0 +1,15 @@
+// Functions to test
+
+function addNumbers(a, b) {
+    return a + b;
+  }
+  
+  function multiplyNumbers(a, b) {
+    return a * b;
+  }
+  
+  module.exports = {
+    addNumbers,
+    multiplyNumbers
+  };
+  
